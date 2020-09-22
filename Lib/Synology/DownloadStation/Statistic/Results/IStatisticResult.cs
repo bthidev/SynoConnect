99d8e4138e89c861c616340dff18869936b3feb5
Benjamin Thieabaut <thiebaut.benjamin@live.fr>
2020-09-22 10:08:01 +0200

    add fake description for compilateur warning

--- Lib/Synology/DownloadStation/Statistic/Results/IStatisticResult.cs
+++ Lib/Synology/DownloadStation/Statistic/Results/IStatisticResult.cs
@@ -4,11 +4,26 @@ using System.Text;
 
 namespace Synology.DownloadStation.Statistic.Results
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public interface IStatisticResult
     {
+        /// <summary>
+        /// 
+        /// </summary>
         int SpeedDownload { get; }
+        /// <summary>
+        /// 
+        /// </summary>
         int SpeedUpload { get; }
+        /// <summary>
+        /// 
+        /// </summary>
         int EmuleSpeedDownload { get; }
+        /// <summary>
+        /// 
+        /// </summary>
         int EmuleSpeedUpload { get; }
     }
 }
--- Lib/Synology/DownloadStation/Task/Parameters/TaskCreateParametersPost.cs
+++ Lib/Synology/DownloadStation/Task/Parameters/TaskCreateParametersPost.cs
@@ -6,6 +6,9 @@ using System.Text;
 
 namespace Synology.DownloadStation.Task.Parameters
 {
+    /// <summary>
+    /// Task create post parameters.
+    /// </summary>
     public class TaskCreateParametersPost : PostParameters
     {
         /// <summary>
